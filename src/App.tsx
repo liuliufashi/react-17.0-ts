a55b75efa630f4e663f414c3b54fa08a5ceee15e
liuliufashi <2276209401@qq.com>
2021-02-10 12:26:34 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Button} from "antd";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IP{
+    nAme:String,
+    nUm:Number,
+    sayHi: ()=>string
 }
+class  App extends React.Component<any, any>{
+        constructor(props:any) {
+            super(props);
+            this.state={
+                data:1
+            }
+        }
+   data:IP={
+        nAme:"123",
+        nUm:0,
+       sayHi:()=>{
+            return "123"
+       }
+    }
+     datas = [
+        'Racing car sprays burning fuel into crowd.',
+        'Japanese princess to wed commoner.',
+        'Australian walks 100km after outback crash.',
+        'Man charged over missing wedding girl.',
+        'Los Angeles battles huge wildfires.',];
+    sel=()=>{
+        this.datas.splice(this.datas.length-1,1)
+        this.setState({})
+    }
+render() {
 
+        return (
+            <>
+                <Button type="primary" size="small"  onClick={()=>{this.sel()}}>
+                    {this.data.nAme}+{this.data.nUm}+{this.data.sayHi()}
+                </Button>
+                {
+                    this.datas.map((item,index)=>{
+                        return <li key={index}>{item}</li>
+                    })
+                }
+            </>
+        );
+    }
+}
 export default App;
