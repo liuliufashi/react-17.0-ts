39ff12bef560bd2f669f3659857c165cdbacab3e
liuliufashi <2276209401@qq.com>
2021-02-18 15:17:26 +0800

    up

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,6 +1,5 @@
 import { createStore ,applyMiddleware } from  "redux";
 import ThunkMiddleware  from  "redux-thunk";
 import  Reducer from  "./reducer";
-//store 仓库
 const store = createStore(Reducer,applyMiddleware(ThunkMiddleware));
 export default  store;
